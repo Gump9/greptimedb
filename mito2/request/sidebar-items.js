f306680549dae3accb85640b885bb69614729087
Gump9 <missyejing1@gmail.com>
2023-09-07 06:51:34 +0000

    Deploying to gh-pages from @ Gump9/greptimedb@b8c50d00aa81e8fb4a4d5e7dabc92401ce437642 🚀

--- mito2/request/sidebar-items.js
+++ mito2/request/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["WorkerRequest"],"fn":["validate_proto_value"],"struct":["CloseRequest","CreateRequest","OpenRequest","RegionOptions","RegionTask","RequestValidator","SenderWriteRequest","WriteRequest"]};
+window.SIDEBAR_ITEMS = {"enum":["BackgroundNotify","DdlRequest","WorkerRequest"],"fn":["validate_proto_value"],"struct":["FlushFailed","FlushFinished","RegionOptions","SenderDdlRequest","SenderWriteRequest","WriteRequest"]};
